c6b5afef4ca2d45fb1798aa3ed15e5b205693b29
PierreNieto <pierre.nieto@hotmail.com>
2025-11-23 18:39:24 +0100

    i forgot to import "math"

--- app/portfolio/plots.py
+++ app/portfolio/plots.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import numpy as np
+import math
 
 # =====================================================
 # GLOBAL SETTINGS
